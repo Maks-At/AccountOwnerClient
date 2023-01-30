fd019d2e9e21cd4fa67162732d521f742c44a35e
Maks_At <maks.atagoziyev@gmail.com>
2023-01-30 15:45:46 +0300

    Add Not Found Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './error-pages/not-found/not-found.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent},
-  {path: '', redirectTo: '/home', pathMatch: 'full'}
-];
+  { path: 'home', component: HomeComponent },
+  { path: '404', component: NotFoundComponent }, 
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/404', pathMatch: 'full' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
