8eedf51d656fb640c7f88ee3e5b143d47c4e575f
Maks_At <maks.atagoziyev@gmail.com>
2023-01-31 11:49:17 +0300

    Enable error handling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'owner', loadChildren: () => import('./owner/owner.module').then(m => m.OwnerModule) },
   { path: '404', component: NotFoundComponent },
-  { path: '500', component: InternalServerComponent}
+  { path: '500', component: InternalServerComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', redirectTo: '/404', pathMatch: 'full' }];
 
