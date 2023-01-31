93b3f52d5caa5c9abd3a9124c264970915140bec
Maks_At <maks.atagoziyev@gmail.com>
2023-01-31 14:15:07 +0300

    Create error modal

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ModalModule } from 'ngx-bootstrap/modal';
+
+import { ErrorModalComponent } from './modals/error-modal/error-modal.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ErrorModalComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    ModalModule.forRoot()
+  ],
+  exports:[
+    ErrorModalComponent
   ]
 })
 export class SharedModule { }
