3592ce775db4372fd79727184af686f9a5c08304
Maks_At <maks.atagoziyev@gmail.com>
2023-02-01 14:48:45 +0300

    Enable Delete Functionality

--- src/app/owner/owner-routing.module.ts
+++ src/app/owner/owner-routing.module.ts
@@ -5,12 +5,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { OwnerListComponent } from './owner-list/owner-list.component';
+import { OwnerDeleteComponent } from './owner-delete/owner-delete.component';
 
 const routes: Routes = [
   { path: 'list', component: OwnerListComponent},
   { path: 'details/:id', component: OwnerDetailsComponent},
   { path: 'create', component: OwnerCreateComponent},
-  { path: 'update/:id', component: OwnerUpdateComponent}
+  { path: 'update/:id', component: OwnerUpdateComponent},
+  { path: 'delete/:id', component: OwnerDeleteComponent}
 ];
 
 @NgModule({
